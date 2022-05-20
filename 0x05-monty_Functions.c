1e48f4cc4eccbfe9ee7c1b43f0fc4a3e3dcec180
Edwin-9603 <egc4611@gmail.com>
2022-05-19 20:06:46 -0500

    New updates add

--- 0x05-monty_Functions.c
+++ 0x05-monty_Functions.c
@@ -10,11 +10,22 @@
 void _add(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp = *stack;
+	stack_t *current = *stack;
+	int count = 0;
 
-	if (tmp == NULL || (*stack)->prev == NULL)
-		add_error(line_number);
+	while (current)
+	{
+		count++;
+		current = current->next;
+	}
 
-	(tmp->prev)->n = tmp->n + (tmp->prev)->n;
+	if (count < 2)
+	{
+		if (tmp == NULL || (*stack)->next == NULL)
+			add_error(line_number);
+	}
+
+	(tmp->next)->n = tmp->n + (tmp->next)->n;
 	_pop(stack, line_number);
 }
 
